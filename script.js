d72d92ab2b0093e5fa32fccef9222f7e4eb7dbe4
Pedro Vinicius <pedropalomino202@gmail.com>
2023-10-20 12:46:56 -0300

    Script_versao001

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+function soma() {
+    var num1 = document.getElementById("n1");
+    var num2 = document.getElementById("n2");
+    var res = document.getElementById("resultado");
+    var v1 = Number(num1.value);
+    var v2 = Number(num2.value);
+    var s = v1 + v2;
+    res.innerHTML = s;
+}
 
+function subtracao() {
+    var num1 = document.getElementById("n1");
+    var num2 = document.getElementById("n2");
+    var res = document.getElementById("resultado");
+    var v1 = Number(num1.value);
+    var v2 = Number(num2.value);
+    var valtot = v1 - v2;
+    res.innerHTML = valtot;
+}
