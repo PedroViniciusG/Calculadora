7fd2897265dfd88d573a7e186078d8363bd5fc79
Pedro Vinicius <pedropalomino202@gmail.com>
2023-10-22 19:19:22 -0300

    Multiplicacao/Divicao_versao003.1

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function soma() {
     var v2 = Number(num2.value);
     var valtolt = v1 + v2;
     res.innerHTML = valtolt;
-    opecSelec.innerHTML = "+";
+    opecSelec.innerHTML = "&#x2b;";
 }
 
 function subtracao() {
@@ -19,5 +19,29 @@ function subtracao() {
     var v2 = Number(num2.value);
     var valtot = v1 - v2;
     res.innerHTML = valtot;
-    opecSelec.innerHTML = "-";
+    opecSelec.innerHTML = "&#8722;";
+}
+
+function multiplicacao() {
+    var num1 = document.getElementById("n1");
+    var num2 = document.getElementById("n2");
+    var res = document.getElementById("resultado");
+    var opecSelec = document.getElementById("operadorSelecionado");
+    var v1 = Number(num1.value);
+    var v2 = Number(num2.value);
+    var valtot = v1 * v2;
+    res.innerHTML = valtot;
+    opecSelec.innerHTML = "&#xd7;";
+}
+
+function divisao() {
+    var num1 = document.getElementById("n1");
+    var num2 = document.getElementById("n2");
+    var res = document.getElementById("resultado");
+    var opecSelec = document.getElementById("operadorSelecionado");
+    var v1 = Number(num1.value);
+    var v2 = Number(num2.value);
+    var valtot = v1 / v2;
+    res.innerHTML = valtot;
+    opecSelec.innerHTML = "&#247;";
 }
