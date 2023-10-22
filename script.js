1ffa07c1fb3869e365e35ac8375e90194af7cb32
Pedro Vinicius <pedropalomino202@gmail.com>
2023-10-22 14:47:53 -0300

    versao003

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ function soma() {
     var num1 = document.getElementById("n1");
     var num2 = document.getElementById("n2");
     var res = document.getElementById("resultado");
+    var opecSelec = document.getElementById("operadorSelecionado");
     var v1 = Number(num1.value);
     var v2 = Number(num2.value);
-    var s = v1 + v2;
-    res.innerHTML = s;
+    var valtolt = v1 + v2;
+    res.innerHTML = valtolt;
+    opecSelec.innerHTML = "+";
 }
 
 function subtracao() {
     var num1 = document.getElementById("n1");
     var num2 = document.getElementById("n2");
     var res = document.getElementById("resultado");
+    var opecSelec = document.getElementById("operadorSelecionado");
     var v1 = Number(num1.value);
     var v2 = Number(num2.value);
     var valtot = v1 - v2;
     res.innerHTML = valtot;
+    opecSelec.innerHTML = "-";
 }
